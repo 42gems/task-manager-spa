431ba0fded42fcf6bfd4a463228ba43e39cc3b8a
--global <dk.winni@gmail.com>
2014-10-21 16:59:44 +0300

    CRUD for projects on back-end side

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,5 +1,44 @@
 class API::ProjectsController < API::BaseController
+  before_actoin :current_project, only: [:show, :edit, :update, :destroy]
+
   def index
     respond_with Project.all
+  end
+
+  def show
+    respond_with @project
+  end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      respond_with project, status: :created, location: project
+    else
+      render json: project.errors, status: 422
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @project.update(project_params)
+      respond_with @project, status: 200
+    else
+      render json: @project.errors, status: 422
+    end
+  end
+
+  def destroy
+    @project.destroy
+    head 204
+  end
+
+  private
+  def current_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:owner_id, :title, :description)
   end
 end
