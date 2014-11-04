a424b946d19f434fcd07c038fe916749a352b5a0
--global <dk.winni@gmail.com>
2014-11-04 17:06:44 +0200

    Fixed bug with irrelevant to the project tasks.

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,6 +1,16 @@
 class API::TasksController < API::BaseController
+  before_action :fetch_project, except: [:edit, :show]
+
+  def index
+    respond_with @project.tasks
+  end
+
   private
   def task_params
     params.require(:task).permit(:project_id, :title, :description, :state, :estimated_time, :time_spent, :deadline)
+  end
+
+  def fetch_project
+    @project = Project.find(params[:project_id])
   end
 end
