70e18e6579fa08ff562e58dde696cc035023e854
Andrii Nikolaiev <nikolayev1990@gmail.com>
2015-01-12 14:08:09 +0200

    Adding first and last name in the register form

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -31,7 +31,7 @@ class API::UsersController < API::BaseController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 
   def fetch_user
