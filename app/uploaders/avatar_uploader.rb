b7b17d602b63db5108da8893fe05c5719becc3df
Andrii Nikolaiev <nikolayev1990@gmail.com>
2015-01-22 20:50:51 +0200

    fixing member tile image height

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   process :convert => 'png'
-  resize_to_fit(50, 50)
+  resize_to_fit(32, 32)
 
   def default_url
   	"/img/" + [version_name, "default_avatar.png"].compact.join('_')
